6ec3ab73dd57bef16a9bc8df51662cd5030104e1
seyiadel <seyiadel03@gmail.com>
2024-09-19 10:56:36 +0100

    fix:db conn refactor

--- src/app.js
+++ src/app.js
@@ -15,16 +15,17 @@ const connectionParams = {
   autoIndex:false,
   dbName:process.env.DATABASE_NAME
 }
-mongoose
-  .connect(MONGO_URI, connectionParams)
-  .then(() => {
-    console.log("Successfully connected to database");
-  })
-  .catch((error) => {
+try{
+  let dbConn = await mongoose.connect(MONGO_URI, connectionParams)
+  if(dbConn ){
+      console.log("Successfully connected to database");
+    }
+}catch(error){
+
     console.log("database connection failed. exiting now...");
     console.error(error);
     process.exit(1);
-  });
+}
 mongoose.set("strictQuery", false);
 
 // middlewares
